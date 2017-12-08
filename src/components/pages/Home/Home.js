149db6f21adde880b2aed688884147f0ebc3a5ae
lightnet328 <lightnet328@gmail.com>
2017-12-09 00:22:34 +0900

    Add Logo

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -4,7 +4,8 @@ import type { ComponentType } from "react";
 import { bindActionCreators } from "redux";
 import type { Dispatch } from "redux";
 import { connect } from "react-redux";
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
+import Logo from "assets/images/react-logo.svg";
 
 const Root: ComponentType<{}> = styled.div`
   width: 100vw;
@@ -15,9 +16,35 @@ const Root: ComponentType<{}> = styled.div`
   justify-content: center;
 `;
 
+const rotate360 = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const duration = 4;
+
+const RotatingLogo = styled(Logo).attrs({
+  width: 128,
+  height: 128
+})`
+  animation: ${rotate360} ${duration}s linear infinite;
+  &:hover {
+    animation-duration: ${duration / 2}s;
+  }
+`;
+
 class Home extends Component<{}> {
   render() {
-    return <Root {...this.props} />;
+    return (
+      <Root {...this.props}>
+        <RotatingLogo />
+      </Root>
+    );
   }
 }
 
