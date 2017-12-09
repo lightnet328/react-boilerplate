694920054ad403a9359bd7f23d05d8e98f5a1b37
lightnet328 <lightnet328@gmail.com>
2017-12-09 23:44:34 +0900

    Add types to model

--- src/models/Calendar/Calendar.js
+++ src/models/Calendar/Calendar.js
@@ -1,23 +1,23 @@
 // @flow
 import { Record } from "immutable";
 
-class Calendar extends Record({
-  date: new Date()
-}) {
-  getYear() {
+class Calendar extends Record({ date: new Date() })<{ date: Date }> {
+  date: Date;
+
+  getYear(): number {
     return this.date.getFullYear();
   }
 
-  getMonth() {
+  getMonth(): number {
     return this.date.getMonth() + 1;
   }
 
-  prevMonth() {
+  prevMonth(): Calendar {
     const [year, month] = [this.getYear(), this.getMonth() - 1];
     return this.set("date", new Date(year, month - 1));
   }
 
-  nextMonth() {
+  nextMonth(): Calendar {
     const [year, month] = [this.getYear(), this.getMonth() - 1];
     return this.set("date", new Date(year, month + 1));
   }
--- src/models/Counter/Counter.js
+++ src/models/Counter/Counter.js
@@ -1,12 +1,14 @@
 // @flow
 import { Record } from "immutable";
 
-class Counter extends Record({ counter: 0 }) {
-  increment() {
+class Counter extends Record({ counter: 0 })<{ counter: number }> {
+  counter: number;
+
+  increment(): Counter {
     return this.set("counter", this.counter + 1);
   }
 
-  decrement() {
+  decrement(): Counter {
     return this.set("counter", this.counter - 1);
   }
 }
